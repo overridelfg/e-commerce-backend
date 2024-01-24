20f15868a2d8e61309110c9e19ce61b43ab04e9e
Rychkov Kirill <36132918+KERELLL@users.noreply.github.com>
2024-01-24 21:26:20 +0100

    fixed pagination

--- e-commerce/src/product/product.service.ts
+++ e-commerce/src/product/product.service.ts
@@ -46,13 +46,15 @@ export class ProductService {
         ]
     }
 
+    const totalSize = (await this.productModel.find(productsQuery)).length;
     const products = await this.productModel.find(productsQuery).skip(skip).limit(perPage).sort(sortBy);
 
+    console.log(products.length)
     const productsListSize = await this.productModel.countDocuments();
 
     return {
       products,
-      length: productsListSize
+      length: totalSize
     };
   }
 
